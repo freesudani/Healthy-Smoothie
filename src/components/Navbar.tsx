cb6f60cfebd431a40528c7697f6f798e9d6f1ed2
freesudani <mohamed.dahiya@hotmail.com>
2022-07-03 09:59:31 +0700

    Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from "react";
+import IconImage from "../images/e153069c5bc343f08a9b8398de94b63c.png";
+
+const Navbar: FC = () => {
+  return (
+    <div className="static w-screen h-48 m-0 px-8 bg-white text-black flex justify-between items-center">
+      <div>
+        <img src={IconImage} alt="Logo" className="h-auto w-44" />
+      </div>
+      <div>
+        <button className="lgbt primarybt uppercase">Contact Us</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+    },
+    extend: {
+      screens: {
+        mb: { max: "400px" },
+        // => @media (max-width: 400px)  { ... }
+      },
+      colors: {
+        primary: { 200: "#0b752c", 100: "#549e6b" },
+        secondary: { 200: "#fa8348", 100: "#faae48" },
+      },
+    },
   },
   plugins: [],
-}
+};
